2ff25a5151171d997345925131f46a0e4f603438
Joshua Koh <63396516+theradongkid2@users.noreply.github.com>
2022-08-04 09:07:26 +1000

    Update calParser.js

--- src/calParser.js
+++ src/calParser.js
@@ -966,6 +966,7 @@ async function icalProcess() {
         localStorage.setItem("timetable", JSON.stringify(timetable)); // Saving the timetable as a JSON string on local host for future use.
         //console.log(timetable);
         alert("Timetable uploaded successfully :)");
+        location.reload();
     } catch(e) {
         alert(e); // If theres an error in the processing prompt the user with a HTML alert with the error in the body.
     }
